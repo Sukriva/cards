934cf03ac27e5b35605ece134d8fd71b773cfd43
Sukriva <rihnu@hotmail.com>
2019-02-07 12:00:20 +0200

    poistettu pari turhaa riviä

--- connect.php
+++ connect.php
@@ -5,8 +5,6 @@ function connect_db() {
  $password = "ryGYkT62m";
  $dbname = "a1704566;
  // Create connection
-GIT Exercises 4 (12)
-29.12.2018
  $conn = new mysqli($servername, $username, $password, $dbname);
  // Check connection
  if ($conn->connect_error) {
